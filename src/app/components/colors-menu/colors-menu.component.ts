dda6104d572b4ba02db4d3d6159d4837c1c5e1f2
edsonsilvajr <edsonsilvajr21@gmail.com>
2024-02-10 14:48:36 -0300

    adicionando opções de cor e emitindo evento

--- src/app/components/colors-menu/colors-menu.component.ts
+++ src/app/components/colors-menu/colors-menu.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-colors-menu',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, NgStyle, NgClass, FormsModule],
   templateUrl: './colors-menu.component.html',
-  styleUrl: './colors-menu.component.css'
+  styleUrl: './colors-menu.component.css',
 })
 export class ColorsMenuComponent {
+  @Output() evento = new EventEmitter();
+  colors = ['#000000', '#ff0000', '#1AFF00', '#0008FF', '#FFF700'];
+  selectedColor = '#000000';
+  faSquare = faSquare;
+  range = 1;
 
+  handleInputChange() {
+    this.evento.emit({
+      color: this.selectedColor,
+      size: this.range,
+    });
+  }
 }
